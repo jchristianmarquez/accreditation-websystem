06f28f721b67184743d8b8d16744745e16229547
John Christian Marquez <jcmarquez012@gmail.com>
2022-07-14 10:07:57 +0800

    fix some errors

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,5 +10,13 @@
         </li>
         <li><a href="#">User Setting</a></li>
         <li><a href="#">Account Setting</a></li>
+        <li>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <a href="route('logout')" onclick="event.preventDefault();this.closest('form').submit();">
+                    Log Out
+                </a>
+            </form>
+        </li>
     </ul>
 </nav>
